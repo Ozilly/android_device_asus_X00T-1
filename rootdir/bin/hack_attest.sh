b209374344edf99c0e4e692f6f932e81e2e73022
ozilly <hellsing890@gmail.com>
2019-09-12 19:06:10 +0300

    X00T: add hack for gpay

---
+++ rootdir/bin/hack_attest.sh
@@ -0,0 +1,23 @@
+#! /system/bin/sh
+
+sleep 120
+statuselinux=`cat /sys/fs/selinux/enforce`
+statuselinux_temp=0
+
+# Attest method hack google pay
+if [ -w /data/data/com.google.android.gms/databases/dg.db ]; then
+
+    if [ "$statuselinux" == "1" ]; then 
+       statuselinux_temp=1
+       setenforce 0 
+    fi
+
+    rm -rf /data/data/com.google.android.gms/app_dg_cache/*
+    sqlite3 -batch /data/data/com.google.android.gms/databases/android_pay "update Wallets set fails_attestation='0'"
+    sqlite3 -batch /data/data/com.google.android.gms/databases/dg.db "update main set c=0 where a like '%attest%'"
+    chmod 440 /data/data/com.google.android.gms/databases/dg.db
+
+    if [ $statuselinux_temp == 1 ]; then
+       setenforce 1
+    fi
+fi
